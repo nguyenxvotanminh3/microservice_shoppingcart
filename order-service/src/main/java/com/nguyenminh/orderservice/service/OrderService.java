2c6d0e2aefcf1c811fbd858c5ef66ddde5630e0e
Nguyễn Minh <91356207+nguyenxvotanminh3@users.noreply.github.com>
2024-02-04 09:15:55 +0700

    add syn communication webclient

--- order-service/src/main/java/com/nguyenminh/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/nguyenminh/orderservice/service/OrderService.java
@@ -7,11 +7,13 @@ import com.nguyenminh.orderservice.model.OrderLineItems;
 import com.nguyenminh.orderservice.repository.OrderRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.UUID;
+
+import static java.lang.Boolean.*;
 
 
 @Service
@@ -21,6 +23,7 @@ public class OrderService {
 
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -32,7 +35,15 @@ public class OrderService {
 
 
         //Call inventory-service and place order
-        orderRepository.save(order);
+        Boolean result = webClient.get()
+                .uri("http://localhost:8082/api/inventory")
+                .retrieve()
+                .bodyToMono(Boolean.class)
+                .block();
+            if(TRUE.equals(result)) orderRepository.save(order);
+            else {
+                throw new IllegalArgumentException("Product is not in stock, try again later");
+            }
     }
 
     private OrderLineItems mapToDto(OrderLineItemDto orderLineItemDto) {
